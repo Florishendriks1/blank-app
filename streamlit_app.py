f9201304f14db36912963b9b6978a73c5367c778
Florishendriks1 <floris.hendriks@hotmail.com>
2025-04-16 14:25:50 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
+import random
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🤣 De Digitale Moppentrommel")
+
+moppen = [
+    "Waarom kunnen geheimagenten niet goed schaken? Ze zijn bang voor openingen.",
+    "Wat zegt een wiskundeleraar tijdens het daten? Jij bent echt mijn type!",
+    "Waarom nam de computer een pauze? Hij had te veel tabs open!",
+    "Wat zegt een boom als hij wordt omgezaagd? ‘Ik voel me geveld…’"
+]
+
+if st.button("Vertel een mop!"):
+    st.success(random.choice(moppen))
